5c380ae56cfea856322730036846097c0dc04efd
Marco Lopes <marco.paulo.lopes@gmail.com>
2012-09-05 19:48:17 +0100

    Update cookbook description

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "Jiva Technology, Ltd."
 maintainer_email "theo@jivatechnology.com"
 license          "Apache 2.0"
-description      "Installs/Configures solr"
+description      "Install and configure solr"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.3"
 
